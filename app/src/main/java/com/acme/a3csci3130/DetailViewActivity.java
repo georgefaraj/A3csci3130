c8cfbcac6c289d165a2a15b575757579cb6fc200
georgefaraj <georgemilio@gmail.com>
2018-06-24 10:00:09 -0300

    George F, Finished updating 'Detail View' activity layout, pending: add functions to the update and erase buttons.

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -3,25 +3,51 @@ package com.acme.a3csci3130;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
-    Business receivedPersonInfo;
+    private EditText nameField, numberField,addressField;
+    private Spinner primaryBField,provinceField;
+    Business receivedBusyInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
-        receivedPersonInfo = (Business)getIntent().getSerializableExtra("Business");
+        receivedBusyInfo = (Business)getIntent().getSerializableExtra("Business");
 
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.number);
+        numberField = (EditText) findViewById(R.id.number);
+        addressField = (EditText) findViewById(R.id.address);
+        primaryBField = (Spinner) findViewById(R.id.primaryB);
+        provinceField = (Spinner) findViewById(R.id.province);
 
-        if(receivedPersonInfo != null){
-            nameField.setText(receivedPersonInfo.name);
-            //emailField.setText(receivedPersonInfo.email);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> PBadapter = ArrayAdapter.createFromResource(this,
+                R.array.business_types, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> PRadapter = ArrayAdapter.createFromResource(this,
+                R.array.provinces, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        PBadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        PRadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        primaryBField.setAdapter(PBadapter);
+        provinceField.setAdapter(PRadapter);
+        int spinnerPosition;
+
+        if(receivedBusyInfo != null){
+            nameField.setText(receivedBusyInfo.name);
+            numberField.setText(String.valueOf( receivedBusyInfo.number ));
+            addressField.setText(receivedBusyInfo.address);
+
+            spinnerPosition = PBadapter.getPosition(receivedBusyInfo.primaryB);
+            primaryBField.setSelection(spinnerPosition);
+
+            spinnerPosition = PRadapter.getPosition(receivedBusyInfo.province);
+            provinceField.setSelection(spinnerPosition);
         }
     }
 
